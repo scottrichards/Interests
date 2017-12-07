42942cee40b080702ec7d67b8e3e52eb1866747d
Scott Richards <scott.richards@glassdoor.com>
2017-12-07 15:29:17 -0800

    Initial Commit

---
+++ Interests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Interests
+//
+//  Created by Scott Richards on 12/7/17.
+//  Copyright © 2017 BitWyze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
